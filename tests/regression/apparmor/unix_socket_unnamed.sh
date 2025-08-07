4fdf2f90d9cf5fd615e913ef2901f7bcf91a685c
John Johansen <john@jjmx.net>
2025-08-07 16:22:31 -0600

    tests: regression: update test requires for v9 af_unix
    Make sure regressions tests for unix sockets will run when v9 af_unix
    is present.
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    (cherry picked from commit 85f71dfb94f987d80af63a408455e2b490fb6526)
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- tests/regression/apparmor/unix_socket_unnamed.sh
+++ tests/regression/apparmor/unix_socket_unnamed.sh
@@ -28,8 +28,8 @@ bin=$pwd
 
 . "$bin/prologue.inc"
 . "$bin/unix_socket.inc"
-requires_kernel_features policy/versions/v7
-requires_kernel_features network/af_unix
+requires_any_of_kernel_features policy/versions/v7 policy/versions/v8 policy/versions/v9
+requires_any_of_kernel_features network/af_unix network_v9/af_unix
 requires_parser_support "unix,"
 
 settest unix_socket
