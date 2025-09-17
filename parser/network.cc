823a8e99157d99fe1e9bfdde04cd909552769e3c
John Johansen <john.johansen@canonical.com>
2025-09-17 08:42:07 -0700

    parser: fix cppcheck warnings for network.cc
    From static analyser cppcheck:
    apparmor-v4.1.2/parser/network.cc:614:49: performance: Function parameter 'ip' should be passed by const reference. [passedByValue]
    apparmor-v4.1.2/parser/network.cc:986:100: performance: Function parameter 'lhs' should be passed by const reference. [passedByValue]
    apparmor-v4.1.2/parser/network.cc:987:79: performance: Function parameter 'rhs' should be passed by const reference. [passedByValue]
    
    The parser could be passing some parameters by constant reference
    instead of by value. This will provide a small performance win.
    
    BugLink: https://gitlab.com/apparmor/apparmor/-/issues/551
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- parser/network.cc
+++ parser/network.cc
@@ -611,7 +611,7 @@ void network_rule::warn_once(const char *name)
 	rule_t::warn_once(name, "network rules not enforced");
 }
 
-std::string gen_ip_cond(const struct ip_address ip)
+std::string gen_ip_cond(struct ip_address const &ip)
 {
 	std::ostringstream oss;
 	int i;
@@ -984,16 +984,16 @@ static int cmp_ip_conds(ip_conds const &lhs, ip_conds const &rhs)
 	return lhs.is_none - rhs.is_none;
 }
 
-static int cmp_network_map(std::unordered_map<unsigned int, std::pair<unsigned int, unsigned int>> lhs,
-			   std::unordered_map<unsigned int, std::pair<unsigned int, unsigned int>> rhs)
+static int cmp_network_map(std::unordered_map<unsigned int, std::pair<unsigned int, unsigned int>> const &lhs,
+			   std::unordered_map<unsigned int, std::pair<unsigned int, unsigned int>> const &rhs)
 {
 	int res;
 	size_t family_index;
 	for (family_index = AF_UNSPEC; family_index < get_af_max(); family_index++) {
-		res = lhs[family_index].first - rhs[family_index].first;
+		res = lhs.at(family_index).first - rhs.at(family_index).first;
 		if (res)
 			return res;
-		res = lhs[family_index].second - rhs[family_index].second;
+		res = lhs.at(family_index).second - rhs.at(family_index).second;
 		if (res)
 			return res;
 	}
