apparmor/apparmor

commit cc1dc33617ea7838dbcc16396b77b8d9382ba0a0

utils: add conditionals support

utils/apparmor/regex.py
@@ -28,7 +28,7 @@ RE_COMMA_EOL = r'\s*,' + RE_EOL  # optional whitespace, comma + RE_EOL
 
 RE_PROFILE_NAME = r'(?P<%s>(\S+|"[^"]+"))'  # string without spaces, or quoted string. %s is the match group name
 RE_PATH = r'/\S*|"/[^"]*"'  # filename (starting with '/') without spaces, or quoted filename.
-RE_VAR = r'@{[^}\s]+}'
+RE_VAR = r'@{?[^}\s]+}?'
 RE_DICT_ENTRY = r'\s*(?P<key>[^,\s=]+)(?:=(?P<value>[^,\s=]+))?\s*'
 RE_PROFILE_PATH = '(?P<%s>(' + RE_PATH + '))'  # quoted or unquoted filename. %s is the match group name
 RE_PROFILE_PATH_OR_VAR = '(?P<%s>(' + RE_PATH + '|' + RE_VAR + r'\S*|"' + RE_VAR + '[^"]*"))'  # quoted or unquoted filename or variable. %s is the match group name
@@ -37,6 +37,10 @@ RE_XATTRS = r'(\s+xattrs\s*=\s*\((?P<xattrs>([^)=]+(=[^)=]+)?\s?)*)\)\s*)?'
 RE_FLAGS = r'(\s+(flags\s*=\s*)?\((?P<flags>[^)]+)\))?'
 
 RE_VARIABLE = re.compile(RE_VAR)
+RE_ID = r'(?P<id%(label)s>[^,!#\s=@$()"]+|"(\w|\s)*")'
+RE_VARIABLES = r'(?P<var%(label)s>(?P<var_type%(label)s>@|\$)\{?(?P<varname%(label)s>\w+)\}?)'
+RE_ID_OR_VAR = r'(' + RE_VARIABLES + r'|' + RE_ID + r')'
+RE_ALL_VARIABLES = re.compile(RE_VARIABLES % {'label': ''})
 
 RE_PROFILE_END = re.compile(r'^\s*\}' + RE_EOL)
 RE_PROFILE_ALL = re.compile(RE_PRIORITY_AUDIT_DENY + r'all' + RE_COMMA_EOL)
@@ -45,9 +49,21 @@ RE_PROFILE_ALIAS = re.compile(r'^\s*alias\s+(?P<orig_path>"??.+?"??)\s+->\s*(?P<
 RE_PROFILE_RLIMIT = re.compile(r'^\s*set\s+rlimit\s+(?P<rlimit>[a-z]+)\s*<=\s*(?P<value>[^ ]+(\s+[a-zA-Z]+)?)' + RE_COMMA_EOL)
 RE_PROFILE_BOOLEAN = re.compile(r'^\s*(?P<varname>\$\{?\w*\}?)\s*=\s*(?P<value>true|false)\s*,?' + RE_EOL, flags=re.IGNORECASE)
 RE_PROFILE_VARIABLE = re.compile(r'^\s*(?P<varname>@\{?\w+\}?)\s*(?P<mode>\+?=)\s*(?P<values>@*.+?)' + RE_EOL)
-RE_PROFILE_CONDITIONAL = re.compile(r'^\s*if\s+(not\s+)?(\$\{?\w*\}?)\s*\{' + RE_EOL)
-RE_PROFILE_CONDITIONAL_VARIABLE = re.compile(r'^\s*if\s+(not\s+)?defined\s+(@\{?\w+\}?)\s*\{\s*(#.*)?$')
-RE_PROFILE_CONDITIONAL_BOOLEAN = re.compile(r'^\s*if\s+(not\s+)?defined\s+(\$\{?\w+\}?)\s*\{\s*(#.*)?$')
+
+RE_BOOLEAN_OP = r'(?P<boolean_op%(term)s>(?P<boolean_not%(term)s>(not\s+)*)(?P<defined%(term)s>defined\s+)?' + RE_VARIABLES % {'label': '%(term)s'} + r')'
+RE_COMPARE_OP_QUOTED = r'(?P<compare_op%(term)s>(?P<compare_not%(term)s>(not\s+)*)(?P<left%(term)s>"?' + RE_ID_OR_VAR % {'label': '_left%(term)s'} + r'"?)\s+(?P<op%(term)s>==|!=|in|>|>=|<|<=)\s+(?P<right%(term)s>"?' + RE_ID_OR_VAR % {'label': '_right%(term)s'} + r'"?))'  # used only by transform_cond
+RE_COMPARE_OP = RE_COMPARE_OP_QUOTED.replace('"?', '')
+
+RE_FACTOR = r'(?P<open_paren%(term)s>\()?(' + RE_COMPARE_OP + r'|' + RE_BOOLEAN_OP + r')(?P<close_paren%(term)s>\))?'
+
+RE_TERM = r'(?P<open_paren%(expr)s>\()*\s*((?P<one%(expr)s>' + RE_FACTOR % {'term': '_1%(expr)s'} + r')\s+(?P<cond_op%(expr)s>and|or)\s+(?P<two%(expr)s>' + RE_FACTOR % {'term': '_2%(expr)s'} + ')|' + RE_FACTOR % {'term': '_0%(expr)s'} + r')\s*(?P<close_paren%(expr)s>\))*'
+
+RE_CONDITION = r'(?P<expr>(?P<open_paren>\()?(?P<first>' + RE_TERM % {'expr': '_first'} + r')(\s+(?P<cond_op>and|or)\s+(?P<second>' + RE_TERM % {'expr': '_second'} + r'))*(?P<close_paren>\))?)'
+
+RE_PROFILE_CONDITIONAL = r'\s*if\s+' + RE_CONDITION + r'\s*\{'
+
+RE_PROFILE_CONDITIONAL_START = re.compile(r'^' + RE_PROFILE_CONDITIONAL + RE_EOL)
+RE_PROFILE_CONDITIONAL_ELSE = re.compile(r'^\s*(?P<close>\})?\s*else((?P<if>\s+' + RE_PROFILE_CONDITIONAL + r')|(\s*\{))' + RE_EOL)
 RE_PROFILE_NETWORK = re.compile(RE_PRIORITY_AUDIT_DENY + r'network(?P<details>\s+.*)?' + RE_COMMA_EOL)
 RE_PROFILE_CHANGE_HAT = re.compile(r'^\s*\^("??.+?"??)' + RE_COMMA_EOL)
 RE_PROFILE_HAT_DEF = re.compile(r'^(?P<leadingspace>\s*)(?P<hat_keyword>\^|hat\s+)(?P<hat>"??[^)]+?"??)' + RE_FLAGS + r'\s*\{' + RE_EOL)

parser/tst/simple_tests/conditional/bad_5.sd
@@ -1,0 +1,10 @@
+#=DESCRIPTION trying to use undefined boolean
+#=EXRESULT FAIL
+
+$FOO=true
+
+/bin/true {
+  if $BAR {
+    /bin/true rix,
+  }
+}

parser/tst/simple_tests/conditional/bad_6.sd
@@ -1,0 +1,9 @@
+#=DESCRIPTION unfinished else
+#=EXRESULT FAIL
+
+$BAR=true
+
+/bin/true {
+  if $BAR {
+    /bin/true rix,
+  } else {

parser/tst/simple_tests/conditional/bad_dup_hats_3.sd
@@ -1,0 +1,19 @@
+#=DESCRIPTION duplicated hats inside a conditional
+#=EXRESULT FAIL
+
+${FOO} = true
+
+/bin/true {
+
+  ^dupehat {
+    /bin/false rix,
+  }
+
+  if not ${FOO} {
+    /bin/true rm,
+  } else {
+    ^dupehat {
+      capability dac_override,
+    }
+  }
+}

parser/tst/simple_tests/conditional/ok_30.sd
@@ -1,0 +1,11 @@
+#=DESCRIPTION conditional within profile
+#=EXRESULT PASS
+
+$FOO=true
+@{BAR}=10
+
+/bin/true {
+  if $FOO and @{BAR} >= 10 {
+      /bin/true rix,
+  }
+}

utils/apparmor/aa.py
@@ -35,13 +35,14 @@ from apparmor.common import (
 from apparmor.profile_list import ProfileList, preamble_ruletypes
 from apparmor.profile_storage import ProfileStorage, add_or_remove_flag, ruletypes
 from apparmor.regex import (
-    RE_HAS_COMMENT_SPLIT, RE_PROFILE_CHANGE_HAT, RE_PROFILE_CONDITIONAL,
-    RE_PROFILE_CONDITIONAL_BOOLEAN, RE_PROFILE_CONDITIONAL_VARIABLE, RE_PROFILE_END,
+    RE_HAS_COMMENT_SPLIT, RE_PROFILE_CHANGE_HAT, RE_PROFILE_CONDITIONAL_START,
+    RE_PROFILE_CONDITIONAL_ELSE, RE_PROFILE_END,
     RE_PROFILE_HAT_DEF, RE_PROFILE_START, RE_METADATA_LOGPROF_SUGGEST,
     RE_RULE_HAS_COMMA, parse_profile_start_line, re_match_include)
 from apparmor.rule.abi import AbiRule
 from apparmor.rule.file import FileRule
 from apparmor.rule.include import IncludeRule
+from apparmor.rule.conditional import ConditionalBlock
 from apparmor.logparser import ReadLog
 from apparmor.translations import init_translation
 
@@ -1789,28 +1790,57 @@ def attach_profile_data(profiles, profile_data):
         profiles[p] = deepcopy(profile_data[p])
 
 
-def parse_profile_data(data, file, do_include, in_preamble):
+def parse_conditional(conditional_block, profile_data, data, lineno, file, do_include, in_preamble, profname, profile, debug_lineno):
+    cond_storage = ProfileStorage(profile_data[profname]['info']['profile'],
+                                  profile_data[profname]['info']['hat'],
+                                  profile_data[profname]['info']['calledby'] + ' in cond')
+    ret_lineno, ret_profile_data, end_of_block = parse_block(data[lineno + 1:], file, do_include, in_preamble, profname, profile, cond_storage, debug_lineno, in_if=True)
+    conditional_block.store_profile_data(ret_profile_data)
+    lineno = lineno + ret_lineno
+
+    if conditional_block.result:
+        for cond_profname in ret_profile_data:
+            if ret_profile_data[cond_profname]['in_cond']:
+                if profile_data.get(cond_profname, False):
+                    cond_prof_info = ret_profile_data[cond_profname].data['info']
+                    cond_prof_name = combine_name(cond_prof_info['profile'], cond_prof_info['hat'])
+                    raise AppArmorException(
+                        'Profile %(profile)s defined twice in %(file)s, last found in line %(line)s'
+                        % {'file': file, 'line': debug_lineno + 1, 'profile': cond_prof_name})
+                profile_data[cond_profname] = ret_profile_data[cond_profname]
+
+    if not end_of_block:
+        i = 1
+        if lineno + i < len(data):
+            next_line = data[lineno + i].strip()
+            matches = RE_PROFILE_CONDITIONAL_ELSE.search(next_line)
+            if matches and matches.group('close'):
+                data[lineno + i] = data[lineno + i].replace('}', '', 1)
+    else:
+        profile_data[profname]['cond_block'].add(conditional_block)
+        conditional_block = None
+
+    return lineno, conditional_block
+
+
+def parse_block(data, file, do_include, in_preamble, profname, profile, prof_storage, src_lineno, in_if=False):
     profile_data = {}
-    profile = None
     hat = None
-    profname = None
     in_contained_hat = None
     parsed_profiles = []
     initial_comment = ''
     lastline = None
+    conditional_block = None
 
-    active_profiles.init_file(file)
-
-    if do_include:
-        profile = file
-        hat = None
-        profname = combine_profname((profile, hat))
-        profile_data[profname] = ProfileStorage(profile, hat, 'parse_profile_data() do_include')
+    if do_include or profname and profname not in profile_data:
+        profile_data[profname] = prof_storage
         profile_data[profname]['filename'] = file
-
-    for lineno, line in enumerate(data):
-        line = line.strip()
+    lineno = 0
+    while lineno < len(data):
+        line = data[lineno].strip()
+        debug_lineno = src_lineno + lineno
         if not line:
+            lineno += 1
             continue
         # we're dealing with a multiline statement
         if lastline:
@@ -1818,11 +1848,13 @@ def parse_profile_data(data, file, do_include, in_preamble):
             lastline = None
 
         # is line handled by a *Rule class?
-        (rule_name, rule_obj) = match_line_against_rule_classes(line, profile, file, lineno, in_preamble)
+        (rule_name, rule_obj) = match_line_against_rule_classes(line, profile, file, debug_lineno, in_preamble)
         if rule_name:
             if in_preamble:
                 active_profiles.add_rule(file, rule_name, rule_obj)
             else:
+                if profname not in profile_data:
+                    profile_data[profname] = prof_storage
                 profile_data[profname][rule_name].add(rule_obj)
 
             if rule_name == 'inc_ie':
@@ -1844,16 +1876,16 @@ def parse_profile_data(data, file, do_include, in_preamble):
 
             in_preamble = False
 
-            (profile, hat, prof_storage) = ProfileStorage.parse(line, file, lineno, profile, hat)
-
+            (profile, hat, prof_storage) = ProfileStorage.parse(line, file, debug_lineno, profile, hat)
             if profile == hat:
                 hat = None
             profname = combine_profname((profile, hat))
 
             if profile_data.get(profname, False):
+                print(file)
                 raise AppArmorException(
                     'Profile %(profile)s defined twice in %(file)s, last found in line %(line)s'
-                    % {'file': file, 'line': lineno + 1, 'profile': combine_name(profile, hat)})
+                    % {'file': file, 'line': debug_lineno + 1, 'profile': combine_name(profile, hat)})
 
             profile_data[profname] = prof_storage
 
@@ -1863,12 +1895,11 @@ def parse_profile_data(data, file, do_include, in_preamble):
 
             initial_comment = ''
 
-        elif RE_PROFILE_END.search(line):
-            # If profile ends and we're not in one
-            if not profile:
-                raise AppArmorException(
-                    _('Syntax Error: Unexpected End of Profile reached in file: %(file)s line: %(line)s')
-                    % {'file': file, 'line': lineno + 1})
+            ret_lineno, ret_profile_data, end_of_block = parse_block(data[lineno + 1:], file, do_include, in_preamble, profname, profile, prof_storage, debug_lineno + 1, in_if)
+            profile_data[profname]['in_cond'] = in_if
+            lineno = lineno + ret_lineno
+
+            profile_data = {**profile_data, **ret_profile_data}
 
             if in_contained_hat:
                 hat = None
@@ -1882,32 +1913,49 @@ def parse_profile_data(data, file, do_include, in_preamble):
 
             initial_comment = ''
 
-        elif RE_PROFILE_CONDITIONAL.search(line):
-            # Conditional Boolean
-            pass
+        elif RE_PROFILE_END.search(line):
+            # If profile ends and we're not in one
+            if not profile:
+                raise AppArmorException(
+                    _('Syntax Error: Unexpected End of Profile reached in file: %(file)s line: %(line)s')
+                    % {'file': file, 'line': debug_lineno + 1})
 
-        elif RE_PROFILE_CONDITIONAL_VARIABLE.search(line):
-            # Conditional Variable defines
-            pass
+            return lineno + 1, profile_data, True
 
-        elif RE_PROFILE_CONDITIONAL_BOOLEAN.search(line):
-            # Conditional Boolean defined
-            pass
+        elif RE_PROFILE_CONDITIONAL_START.search(line):
+            conditional_block = ConditionalBlock(line, active_profiles.files[file])
+            lineno, conditional_block = parse_conditional(conditional_block, profile_data, data, lineno, file, do_include, in_preamble, profname, profile, debug_lineno + 1)
+
+        elif RE_PROFILE_CONDITIONAL_ELSE.search(line):
+            if not in_if and conditional_block is None:
+                raise AppArmorException(_('Syntax Error: Unexpected else without previous if in file: %(file)s line: %(line)s')
+                                        % {'file': file, 'line': debug_lineno + 1})
+
+            matches = RE_PROFILE_CONDITIONAL_ELSE.search(line)
+            if matches.group('close'):
+                return lineno, profile_data, False  # not returning next line on purpose
+
+            if conditional_block is None:
+                raise AppArmorException(_('Syntax Error: Unexpected else found without previous if in file: %(file)s line: %(line)s')
+                                        % {'file': file, 'line': debug_lineno + 1})
+            conditional_block.add_conditional(line, active_profiles.files[file])
+            lineno, conditional_block = parse_conditional(conditional_block, profile_data, data, lineno, file, do_include, in_preamble, profname, profile, debug_lineno + 1)
 
         elif RE_PROFILE_CHANGE_HAT.search(line):
             matches = RE_PROFILE_CHANGE_HAT.search(line).groups()
 
             if not profile:
                 raise AppArmorException(_('Syntax Error: Unexpected change hat declaration found in file: %(file)s line: %(line)s')
-                                        % {'file': file, 'line': lineno + 1})
+                                        % {'file': file, 'line': debug_lineno + 1})
 
             aaui.UI_Important(_('Ignoring no longer supported change hat declaration "^%(hat)s," found in file: %(file)s line: %(line)s')
-                              % {'hat': matches[0], 'file': file, 'line': lineno + 1})
+                              % {'hat': matches[0], 'file': file, 'line': debug_lineno + 1})
 
         elif line.startswith('#'):
             # Handle initial comments
             if not profile:
                 if line.startswith('# Last Modified:'):
+                    lineno += 1
                     continue
                 else:
                     initial_comment = initial_comment + line + '\n'
@@ -1931,14 +1979,16 @@ def parse_profile_data(data, file, do_include, in_preamble):
         else:
             raise AppArmorException(
                 _('Syntax Error: Unknown line found in file %(file)s line %(lineno)s:\n    %(line)s')
-                % {'file': file, 'lineno': lineno + 1, 'line': line})
+                % {'file': file, 'lineno': debug_lineno, 'line': line})
+
+        lineno += 1
 
     if lastline:
         # lastline gets merged into line (and reset to None) when reading the next line.
         # If it isn't empty, this means there's something unparsable at the end of the profile
         raise AppArmorException(
             _('Syntax Error: Unknown line found in file %(file)s line %(lineno)s:\n    %(line)s')
-            % {'file': file, 'lineno': lineno + 1, 'line': lastline})
+            % {'file': file, 'lineno': debug_lineno + 1, 'line': lastline})
 
     # Below is not required I'd say
     if not do_include:
@@ -1957,6 +2007,30 @@ def parse_profile_data(data, file, do_include, in_preamble):
         raise AppArmorException(
             _("Syntax Error: Missing '}' or ','. Reached end of file %(file)s while inside profile %(profile)s")
             % {'file': file, 'profile': profile})
+
+    return lineno, profile_data, False
+
+
+def parse_profile_data(data, file, do_include, in_preamble):
+    profile_data = {}
+    profile = None
+    hat = None
+    profname = None
+    prof_storage = None
+
+    active_profiles.init_file(file)
+
+    if do_include:
+        profile = file
+        hat = None
+        profname = combine_profname((profile, hat))
+        prof_storage = ProfileStorage(profile, hat, 'parse_profile_data() do_include')
+
+    _lineno, profile_data, end_of_block = parse_block(data, file, do_include, in_preamble, profname, profile, prof_storage, 0)
+
+    for prof in list(profile_data):  # get the keys
+        if profile_data[prof]['in_cond']:
+            profile_data.pop(prof)
 
     return profile_data
 

utils/apparmor/profile_storage.py
@@ -35,6 +35,7 @@ from apparmor.rule.io_uring import IOUringRule, IOUringRuleset
 from apparmor.rule.mount import MountRule, MountRuleset
 from apparmor.rule.pivot_root import PivotRootRule, PivotRootRuleset
 from apparmor.rule.unix import UnixRule, UnixRuleset
+from apparmor.rule.conditional import ConditionalBlock, ConditionalBlockset
 
 from apparmor.translations import init_translation
 
@@ -58,6 +59,7 @@ ruletypes = {
     'mount':          {'rule': MountRule,         'ruleset': MountRuleset},
     'pivot_root':     {'rule': PivotRootRule,     'ruleset': PivotRootRuleset},
     'unix':           {'rule': UnixRule,          'ruleset': UnixRuleset},
+    'cond_block':     {'rule': ConditionalBlock,  'ruleset': ConditionalBlockset},
 }
 
 
@@ -88,6 +90,7 @@ class ProfileStorage:
         data['profile_keyword'] = False
         data['is_hat'] = False  # profile or hat?
         data['hat_keyword'] = False  # True for 'hat foo', False for '^foo'
+        data['in_cond'] = False  # used for profiles created inside conditionals
 
         self.data = data
 
@@ -178,6 +181,7 @@ class ProfileStorage:
             'inc_ie',
             'rlimit',
             'capability',
+            'cond_block',
             'network',
             'dbus',
             'mount',

utils/apparmor/rule/boolean.py
@@ -51,7 +51,7 @@ class BooleanRule(BaseRule):
             raise AppArmorException('Passed invalid value to %s: %s' % (self.__class__.__name__, value))
 
         self.varname = varname
-        self.value = value
+        self.value = value == 'true'
 
     @classmethod
     def _create_instance(cls, raw_rule, matches):
@@ -70,7 +70,7 @@ class BooleanRule(BaseRule):
 
         space = '  ' * depth
 
-        return '%s%s = %s' % (space, self.varname, self.value)
+        return '%s%s = %s' % (space, self.varname, str(self.value).lower())
 
     def _is_covered_localvars(self, other_rule):
         """check if other_rule is covered by this rule object"""
@@ -78,7 +78,10 @@ class BooleanRule(BaseRule):
         if self.varname != other_rule.varname:
             return False
 
-        if not self._is_covered_list(self.value, None, set(other_rule.value), None, 'value'):
+        if type(self.value) is not bool or type(other_rule.value) is not bool:
+            raise AppArmorBug(_('Invalid value for boolean variable'))
+
+        if self.value != other_rule.value:
             return False
 
         # still here? -> then it is covered

commit a702d70a0301b48dcc27df99259fe5e7b9c82726

utils: fix regex for variable usage
On commit cc1dc336, an incorrect assumption was made regarding
variables: while the variable definition does not need to have
open/closing brackets, their usage, according to the parser, does. The
regex variable RE_VAR and RE_VARIABLE represent only the variable
being used, not its definition, so remove ? not making it optional
anymore.

Fixes: cc1dc336 ("utils: add conditionals support")
Signed-off-by: Georgia Garcia <georgia.garcia@canonical.com>

utils/apparmor/regex.py
@@ -28,7 +28,7 @@ RE_COMMA_EOL = r'\s*,' + RE_EOL  # optional whitespace, comma + RE_EOL
 
 RE_PROFILE_NAME = r'(?P<%s>(\S+|"[^"]+"))'  # string without spaces, or quoted string. %s is the match group name
 RE_PATH = r'/\S*|"/[^"]*"'  # filename (starting with '/') without spaces, or quoted filename.
-RE_VAR = r'@{?[^}\s]+}?'
+RE_VAR = r'@{[^}\s]+}'
 RE_DICT_ENTRY = r'\s*(?P<key>[^,\s=]+)(?:=(?P<value>[^,\s=]+))?\s*'
 RE_PROFILE_PATH = '(?P<%s>(' + RE_PATH + '))'  # quoted or unquoted filename. %s is the match group name
 RE_PROFILE_PATH_OR_VAR = '(?P<%s>(' + RE_PATH + '|' + RE_VAR + r'\S*|"' + RE_VAR + '[^"]*"))'  # quoted or unquoted filename or variable. %s is the match group name

utils/test/test-regex_matches.py
@@ -791,6 +791,7 @@ class TestResolveVariables(AATest):
         ('/@{e}/',      ['/AAA/']),
         ('/@{f}/',      ['/AAABBB/', '/CC/', '/CCC/']),
         ('@{g}',        ['/bin/AAABBB/{foo,bar}', '/bin/CC/{foo,bar}', '/bin/CCC/{foo,bar}']),
+        ('@nota{var}',  ['@nota{var}']),  # not a variable to be expanded
     )
 
     def _run_test(self, params, expected):