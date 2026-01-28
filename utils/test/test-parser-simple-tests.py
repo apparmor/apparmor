c2c04c9b022d9edacd0f0c8cfb260830ab3e2024
Maxime Bélair <maxime.belair@canonical.com>
2026-01-28 16:30:10 +0100

    Add simple tests for identity delegation
    Signed-off-by: Maxime Bélair <maxime.belair@canonical.com>

--- utils/test/test-parser-simple-tests.py
+++ utils/test/test-parser-simple-tests.py
@@ -36,6 +36,9 @@ skip_startswith = (
     # Pux and Cux (which actually mean PUx and CUx) get rejected by the tools
     'generated_x/exact-',
 
+    # identities support not yet implemented in tools
+    'profile/identities/',
+
     # profiles that use undefined autovars like @{exec_path} when no attachment
     'vars/vars_auto_exec_path_bad_2.sd',
     'vars/vars_auto_attach_path_bad_2.sd',
