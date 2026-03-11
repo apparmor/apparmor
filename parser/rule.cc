a10366e9b8e04dc6d09a9f1713993197fe9f4272
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2026-03-11 12:53:17 +0100

    parser: name ns_domain and network_v9 classes
    Replace placeholder class table entries unknown8 and unknown15 with their intended names in aa_class_table.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- parser/rule.cc
+++ parser/rule.cc
@@ -28,14 +28,14 @@ const char *aa_class_table[] = {
 	"rlimit",
 	"domain",
 	"mount",
-	"unknown8", // XXX: "ns_domain"?
+	"ns_domain",
 	"ptrace",
 	"signal",
 	"xmatch",
 	"env",
 	"argv",
 	"network",
-	"unknown15", // XXX: "network_v9"?
+	"network_v9",
 	"label",
 	"mqueue", // XXX: "mqueue_posix"?
 	"mqueue", // XXX: "mqueue_sysv"?
