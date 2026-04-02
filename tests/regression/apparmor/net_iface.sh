b0995c3f451bb67bfe9eabe59d0b9054f5753b53
Georgia Garcia <georgia.garcia@canonical.com>
2026-04-02 20:34:42 -0300

    tests: fix iface tests
    Due to how tap devices are handled in the kernel, they show up as
    loopback devices. The policy generated should be updated to use the
    same name as is expected by the kernel
    
    Signed-off-by: Georgia Garcia <georgia.garcia@canonical.com>

--- tests/regression/apparmor/net_iface.sh
+++ tests/regression/apparmor/net_iface.sh
@@ -45,34 +45,28 @@ remote_ipv6=2001:db8:acad::a0f9
 bind_ipv4=203.0.113.10
 remote_ipv4=203.0.113.200
 
-interface0=tapaatest0
-interface1=tapaatest1
+iface=tapaatest0
+iface_policy=lo # workaround: tap interfaces show up as loopback in the kernel
 
 socat_pids=()
-for iface in $interface0 $interface1; do
-	ip tuntap add dev $iface mode tap
-	sysctl -w net.ipv6.conf.$iface.accept_dad=0 >/dev/null # prevent ipv6 from being tentative
-	ip addr add $bind_ipv4/24 dev $iface
-	ip addr add $remote_ipv4/24 dev $iface
-	ip -6 addr add $bind_ipv6/64 dev $iface
-	ip -6 addr add $remote_ipv6/64 dev $iface
-	ip link set $iface up
-	socat /dev/null,ignoreeof TUN,tun-name=$iface,iff-up=1,tun-type=tap 2>/dev/null &
-	socat_pids+=($!)
-	sleep 0.5
-done
-
+ip tuntap add dev $iface mode tap
+sysctl -w net.ipv6.conf.$iface.accept_dad=0 >/dev/null # prevent ipv6 from being tentative
+ip addr add $bind_ipv4/24 dev $iface
+ip addr add $remote_ipv4/24 dev $iface
+ip -6 addr add $bind_ipv6/64 dev $iface
+ip -6 addr add $remote_ipv6/64 dev $iface
+ip link set $iface up
+socat /dev/null,ignoreeof TUN,tun-name=$iface,iff-up=1,tun-type=tap 2>/dev/null &
+socat_pids+=($!)
 
 cleanup()
 {
 	for pid in ${socat_pids[@]}; do kill $pid; done
-	for iface in $interface0 $interface1; do
-		ip tuntap del dev $iface mode tap
-		ip link show $iface 2>/dev/null
-		if [ $? -eq 0 ]; then
-			echo "Failure removing tap interface $iface"
-		fi
-	done
+	ip tuntap del dev $iface mode tap
+	ip link show $iface 2>/dev/null
+	if [ $? -eq 0 ]; then
+	    echo "Failure removing tap interface $iface"
+	fi
 }
 
 do_onexit="cleanup"
@@ -87,16 +81,16 @@ do_test()
 
 do_tests()
 {
-	prefix="$1"
-	expect_rcv=$2
-	expect_snd=$3
-	bind_ip=$4
-	bind_port=$5
-	remote_ip=$6
-	remote_port=$7
-	protocol=$8
-	iface=$9
-	generate_profile=${10}
+	local prefix="$1"
+	local expect_rcv=$2
+	local expect_snd=$3
+	local bind_ip=$4
+	local bind_port=$5
+	local remote_ip=$6
+	local remote_port=$7
+	local protocol=$8
+	local iface=$9
+	local generate_profile=${10}
 
 	settest net_inet_rcv
 	$generate_profile
@@ -119,82 +113,94 @@ while lsof -i:$remote_port >/dev/null; do
 	let remote_port=$remote_port+1
 done
 
-#TODO: CHANGE XFAILS TO FAIL
-
-for prot in udp tcp; do
-	# make sure both interfaces work
+for prot in tcp udp; do
 	generate_profile=""
-	do_tests "ipv4 $prot $interface0 unconfined" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $interface0 "$generate_profile"
-	do_tests "ipv4 $prot $interface1 unconfined" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $interface1 "$generate_profile"
+	do_tests "ipv4 $prot $iface unconfined" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $iface "$generate_profile"
+
+        # showing denial when it shouldnt for tcp 
+        generate_profile="genprofile flag:debug network:inet:interface=lo $sender:px -- image=$sender network:inet:interface=lo flag:debug"
+        do_tests "ipv4 $prot specific interface" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $iface "$generate_profile"
+
+        generate_profile="genprofile flag:debug network:inet:interface=invalid $sender:px -- image=$sender network:inet:interface=lo flag:debug"
+        do_tests "ipv4 $prot specific interface" fail fail $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $iface "$generate_profile"
+
+	generate_profile="genprofile network:inet qual=deny:network;interface=invalid $sender:px -- image=$sender network:inet qual=deny:network;interface=invalid"
+	do_tests "ipv4 $prot invalid iface, full net perms" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $iface "$generate_profile"
+	
+	generate_profile="genprofile network:inet qual=deny:network;interface=$iface_policy $sender:px -- image=$sender network:inet qual=deny:network;interface=$iface_policy"
+	do_tests "ipv4 $prot valid $iface, full net perms" fail fail $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $iface "$generate_profile"
+
 
 	generate_profile="genprofile network $sender:px -- image=$sender network"
-	do_tests "ipv4 $prot $interface0 no conds" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $interface0 "$generate_profile"
-	generate_profile="genprofile network $sender:px -- image=$sender network"
-	do_tests "ipv4 $prot $interface1 no conds" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $interface1 "$generate_profile"
+	do_tests "ipv4 $prot $iface no conds" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $iface "$generate_profile"
 
-	generate_profile="genprofile network;interface=$interface0 $sender:px -- image=$sender network;interface=$interface0"
-	do_tests "ipv4 $prot $interface0 no conds" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $interface0 "$generate_profile"
-	do_tests "ipv4 $prot $interface1 no conds" xfail xfail $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $interface1 "$generate_profile"
+	generate_profile="genprofile network;interface=$iface_policy $sender:px -- image=$sender network;interface=$iface_policy"
+	do_tests "ipv4 $prot $iface no conds" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $iface "$generate_profile"
 
 	#=
-	setsockopt_rules="network;(setopt,getopt);ip=0.0.0.0;port=0;interface=$interface0" # INADDR_ANY
-	rcv_rules="network;ip=$bind_ipv4;interface=$interface0;peer=(ip=none)"
-	snd_rules="network;ip=$remote_ipv4;interface=$interface0;peer=(ip=none)"
+	setsockopt_rules="network;(setopt,getopt);ip=0.0.0.0;port=0;interface=$iface_policy" # INADDR_ANY
+	rcv_rules="network;ip=$bind_ipv4;interface=$iface_policy;peer=(ip=none)"
+	inv_rcv_rules="network;ip=$bind_ipv4;interface=invalid;peer=(ip=none)"
+	snd_rules="network;ip=$remote_ipv4;interface=$iface_policy;peer=(ip=none)"
+	inv_snd_rules="network;ip=$remote_ipv4;interface=invalid;peer=(ip=none)"
 
 	# port range tests
 	let invalid1=$bind_port-1
-	let end_range=$bind_port+10
-	let invalid2=$bind_port+11
+	let end_range=$bind_port+2
+	let invalid2=$bind_port+3
 
 	for test_port in $(seq $bind_port $end_range); do
-	    generate_profile="genprofile network;ip=$bind_ipv4;port=$bind_port-$end_range;interface=$interface0 $setsockopt_rules $sender:px -- image=$sender network $setsockopt_rules $snd_rules"
-	    do_tests "ipv4 $prot $interface0 port range $test_port generic perms" pass pass $bind_ipv4 $test_port $remote_ipv4 $remote_port $prot $interface0 "$generate_profile"
-	    do_tests "ipv4 $prot $interface1 port range $test_port generic perms" xfail xfail $bind_ipv4 $test_port $remote_ipv4 $remote_port $prot $interface1 "$generate_profile"
+	    generate_profile="genprofile network;ip=$bind_ipv4;port=$bind_port-$end_range;interface=$iface_policy $setsockopt_rules $sender:px -- image=$sender network $setsockopt_rules $snd_rules"
+	    do_tests "ipv4 $prot $iface port range $test_port generic perms" pass pass $bind_ipv4 $test_port $remote_ipv4 $remote_port $prot $iface "$generate_profile"
+	    generate_profile="genprofile network;ip=$bind_ipv4;port=$bind_port-$end_range;interface=invalid $setsockopt_rules $sender:px -- image=$sender network $setsockopt_rules $inv_snd_rules"
+	    do_tests "ipv4 $prot invalid port range $test_port generic perms" fail fail $bind_ipv4 $test_port $remote_ipv4 $remote_port $prot $iface "$generate_profile"
 	done
 
-	generate_profile="genprofile network;ip=$bind_ipv4;port=$bind_port-$end_range;interface=$interface0 $setsockopt_rules $sender:px -- image=$sender network $setsockopt_rules $snd_rules"
-	do_tests "ipv4 $prot port range $invalid1 generic perms" xfail xfail $bind_ipv4 $invalid1 $remote_ipv4 $remote_port $prot $interface0 "$generate_profile"
+	generate_profile="genprofile network;ip=$bind_ipv4;port=$bind_port-$end_range;interface=$iface_policy $setsockopt_rules $sender:px -- image=$sender network $setsockopt_rules $snd_rules"
+	do_tests "ipv4 $prot port range $invalid1 generic perms" fail fail $bind_ipv4 $invalid1 $remote_ipv4 $remote_port $prot $iface "$generate_profile"
 
-	generate_profile="genprofile network;ip=$bind_ipv4;port=$bind_port-$end_range;interface=$interface0 $setsockopt_rules $sender:px -- image=$sender network $setsockopt_rules $snd_rules"
-	do_tests "ipv4 $prot port range $invalid2 generic perms" xfail xfail $bind_ipv4 $invalid2 $remote_ipv4 $remote_port $prot $interface0 "$generate_profile"
+	generate_profile="genprofile network;ip=$bind_ipv4;port=$bind_port-$end_range;interface=$iface_policy $setsockopt_rules $sender:px -- image=$sender network $setsockopt_rules $snd_rules"
+	do_tests "ipv4 $prot port range $invalid2 generic perms" fail fail $bind_ipv4 $invalid2 $remote_ipv4 $remote_port $prot $iface "$generate_profile"
 	# end of port range tests
 
-	generate_profile="genprofile network;ip=$bind_ipv4;port=$bind_port;interface=$interface0;peer=(ip=$remote_ipv4,port=$remote_port) $setsockopt_rules $rcv_rules $sender:px -- image=$sender network;ip=$remote_ipv4;port=$remote_port;peer=(ip=$bind_ipv4,port=$bind_port) $setsockopt_rules $snd_rules"
-	do_tests "ipv4 $prot $interface0 generic perms" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $interface0 "$generate_profile"
-	do_tests "ipv4 $prot $interface1 generic perms" xfail xfail $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $interface1 "$generate_profile"
+	generate_profile="genprofile network;ip=$bind_ipv4;port=$bind_port;interface=$iface_policy;peer=(ip=$remote_ipv4,port=$remote_port) $setsockopt_rules $rcv_rules $sender:px -- image=$sender network;ip=$remote_ipv4;port=$remote_port;peer=(ip=$bind_ipv4,port=$bind_port) $setsockopt_rules $snd_rules"
+	do_tests "ipv4 $prot $iface generic perms" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $iface "$generate_profile"
+	generate_profile="genprofile network;ip=$bind_ipv4;port=$bind_port;interface=invalid;peer=(ip=$remote_ipv4,port=$remote_port) $setsockopt_rules $inv_rcv_rules $sender:px -- image=$sender network;ip=$remote_ipv4;port=$remote_port;peer=(ip=$bind_ipv4,port=$bind_port) $setsockopt_rules $inv_snd_rules"
+	do_tests "ipv4 $prot invalid generic perms" fail fail $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $iface "$generate_profile"
 
-	generate_profile="genprofile network;(connect,receive,send);ip=$bind_ipv4;port=$bind_port;interface=$interface0;peer=(ip=$remote_ipv4,port=$remote_port) $setsockopt_rules $rcv_rules $sender:px -- image=$sender network;ip=$remote_ipv4;port=$remote_port;peer=(ip=$bind_ipv4,port=$bind_port) $setsockopt_rules $snd_rules"
-	do_tests "ipv4 $prot $interface0 specific perms" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $interface0 "$generate_profile"
-	do_tests "ipv4 $prot $interface1 specific perms" xfail xfail $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $interface1 "$generate_profile"
+	generate_profile="genprofile network;(connect,receive,send);ip=$bind_ipv4;port=$bind_port;interface=$iface_policy;peer=(ip=$remote_ipv4,port=$remote_port) $setsockopt_rules $rcv_rules $sender:px -- image=$sender network;ip=$remote_ipv4;port=$remote_port;peer=(ip=$bind_ipv4,port=$bind_port) $setsockopt_rules $snd_rules"
+	do_tests "ipv4 $prot $iface specific perms" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $iface "$generate_profile"
+	generate_profile="genprofile network;(connect,receive,send);ip=$bind_ipv4;port=$bind_port;interface=invalid;peer=(ip=$remote_ipv4,port=$remote_port) $setsockopt_rules $inv_rcv_rules $sender:px -- image=$sender network;ip=$remote_ipv4;port=$remote_port;peer=(ip=$bind_ipv4,port=$bind_port) $setsockopt_rules $inv_snd_rules"
+	do_tests "ipv4 $prot invalid specific perms" fail fail $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $iface "$generate_profile"
 
 
 	removeprofile
 
 	# ipv6 tests
-	# make sure both interfaces work
 	generate_profile=""
-	do_tests "ipv6 $prot $interface0 unconfined" pass pass $bind_ipv6 $bind_port $remote_ipv6 $remote_port $prot $interface0 "$generate_profile"
-	do_tests "ipv6 $prot $interface1 unconfined" pass pass $bind_ipv6 $bind_port $remote_ipv6 $remote_port $prot $interface1 "$generate_profile"
+	do_tests "ipv6 $prot $iface unconfined" pass pass $bind_ipv6 $bind_port $remote_ipv6 $remote_port $prot $iface "$generate_profile"
 
 	generate_profile="genprofile network $sender:px -- image=$sender network"
-	do_tests "ipv6 $prot $interface0 no conds" pass pass $bind_ipv6 $bind_port $remote_ipv6 $remote_port $prot $interface0 "$generate_profile"
-	do_tests "ipv6 $prot $interface1 no conds" pass pass $bind_ipv6 $bind_port $remote_ipv6 $remote_port $prot $interface1 "$generate_profile"
+	do_tests "ipv6 $prot $iface no conds" pass pass $bind_ipv6 $bind_port $remote_ipv6 $remote_port $prot $iface "$generate_profile"
 
-	setsockopt_rules="network;(setopt,getopt);ip=::0;port=0;interface=$interface0" # IN6ADDR_ANY_INIT
-	rcv_rules="network;ip=$bind_ipv6;interface=$interface0;peer=(ip=none)"
-	snd_rules="network;ip=$remote_ipv6;interface=$interface0;peer=(ip=none)"
+	setsockopt_rules="network;(setopt,getopt);ip=::0;port=0;interface=$iface_policy" # IN6ADDR_ANY_INIT
+	rcv_rules="network;ip=$bind_ipv6;interface=$iface_policy;peer=(ip=none)"
+	inv_rcv_rules="network;ip=$bind_ipv6;interface=invalid;peer=(ip=none)"
+	snd_rules="network;ip=$remote_ipv6;interface=$iface_policy;peer=(ip=none)"
+	inv_snd_rules="network;ip=$remote_ipv6;interface=invalid;peer=(ip=none)"
 
-	generate_profile="genprofile network;ip=$bind_ipv6;port=$bind_port;interface=$interface0;peer=(ip=$remote_ipv6,port=$remote_port) $setsockopt_rules $rcv_rules $sender:px -- image=$sender network;ip=$remote_ipv6;port=$remote_port;interface=$interface0;peer=(ip=$bind_ipv6,port=$bind_port) $setsockopt_rules $snd_rules"
-	do_tests "ipv6 $prot $interface0 generic perms" pass pass $bind_ipv6 $bind_port $remote_ipv6 $remote_port $prot $interface0 "$generate_profile"
-	do_tests "ipv6 $prot $interface1 generic perms" xfail xfail $bind_ipv6 $bind_port $remote_ipv6 $remote_port $prot $interface1 "$generate_profile"
+	generate_profile="genprofile network;ip=$bind_ipv6;port=$bind_port;interface=$iface_policy;peer=(ip=$remote_ipv6,port=$remote_port) $setsockopt_rules $rcv_rules $sender:px -- image=$sender network;ip=$remote_ipv6;port=$remote_port;interface=$iface_policy;peer=(ip=$bind_ipv6,port=$bind_port) $setsockopt_rules $snd_rules"
+	do_tests "ipv6 $prot $iface generic perms" pass pass $bind_ipv6 $bind_port $remote_ipv6 $remote_port $prot $iface "$generate_profile"
+	generate_profile="genprofile network;ip=$bind_ipv6;port=$bind_port;interface=invalid;peer=(ip=$remote_ipv6,port=$remote_port) $setsockopt_rules $inv_rcv_rules $sender:px -- image=$sender network;ip=$remote_ipv6;port=$remote_port;interface=invalid;peer=(ip=$bind_ipv6,port=$bind_port) $setsockopt_rules $inv_snd_rules"
+	do_tests "ipv6 $prot invalid generic perms" fail fail $bind_ipv6 $bind_port $remote_ipv6 $remote_port $prot $iface "$generate_profile"
 
 
 	if [ "$(parser_supports 'all,')" = "true" ]; then
 	    generate_profile="genprofile all -- image=$sender all"
-	    do_tests "ipv4 $prot allow all" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $interface0 "$generate_profile"
+	    do_tests "ipv4 $prot allow all" pass pass $bind_ipv4 $bind_port $remote_ipv4 $remote_port $prot $iface "$generate_profile"
 
 	    generate_profile="genprofile all -- image=$sender all"
-	    do_tests "ipv6 $prot allow all" pass pass $bind_ipv6 $bind_port $remote_ipv6 $remote_port $prot $interface0 "$generate_profile"
+	    do_tests "ipv6 $prot allow all" pass pass $bind_ipv6 $bind_port $remote_ipv6 $remote_port $prot $iface "$generate_profile"
 	fi
 
 	# needs to be done for the next loop iteration
