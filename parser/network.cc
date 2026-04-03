apparmor/apparmor

commit 5ae09cb9821b496b68eef593bb7673feaf76e28a

parser: fix label match for default label values
After default_match_pattern for label, but before the null transition
we need to add the full permission set because the kernel requires the
perms to match on the label. The indication that the match may need to
continue being done is done on the permissions of the null transition,
as it's expected by the kernel.

(cherry picked from commit fff3df88741e8b939483fd7f184c7697240bef22)
Signed-off-by: John Johansen <john.johansen@canonical.com>

parser/network.cc
@@ -828,6 +828,21 @@ bool network_rule::gen_ip_conds(Profile &prof, std::list<std::ostringstream> &st
 				lperms = map_perms(perms);
 
 			oss << default_match_pattern; /* label - not used for now */
+
+			buf = oss.str();
+			if (is_iface && !entry.label_match) {
+				/* mandatory perms at this point for
+				 * skb because default label has to
+				 * match actual perms.
+				 * AA_COMPAT_CONT_MATCH is checked on
+				 * the null transition */
+				if (!prof.policy.rules->add_rule(buf.c_str(), priority,
+								 rule_mode, map_perms(perms),
+								 dedup_perms_rule_t::audit == audit_t::FORCE ? map_perms(perms) : 0,
+								 parseopts))
+					return false;
+			}
+
 			oss << "\\x00"; /* null transition */
 
 			buf = oss.str();

commit a1b80134c60d4326602d021e8f5b3beca941af76

parser: fix FTBFS due to missing merge edit
When 5ae09cb98 parser: fix label match for default label values
was backported one of the edits was missed from being added to the
commit, resultng in local builds but failed upstream builds.

Add the missing edit

Fixes: 5ae09cb98 ("parser: fix label match for default label values")
Signed-off-by: John Johansen <john.johansen@canonical.com>

parser/network.cc
@@ -838,7 +838,7 @@ bool network_rule::gen_ip_conds(Profile &prof, std::list<std::ostringstream> &st
 				 * the null transition */
 				if (!prof.policy.rules->add_rule(buf.c_str(), priority,
 								 rule_mode, map_perms(perms),
-								 dedup_perms_rule_t::audit == audit_t::FORCE ? map_perms(perms) : 0,
+								 dedup_perms_rule_t::audit == AUDIT_FORCE ? map_perms(perms) : 0,
 								 parseopts))
 					return false;
 			}