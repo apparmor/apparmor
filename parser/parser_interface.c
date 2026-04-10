c2b2caf6751f2645aeab1826f1fb6c4e97721982
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2026-04-10 11:38:54 +0200

    parser: const correctness for sd_write_blob
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- parser/parser_interface.c
+++ parser/parser_interface.c
@@ -259,7 +259,7 @@ static inline void sd_write_name(std::ostringstream &buf, const char *name)
 	}
 }
 
-static inline void sd_write_blob(std::ostringstream &buf, void *b, int buf_size, char *name)
+static inline void sd_write_blob(std::ostringstream &buf, const void *b, int buf_size, const char *name)
 {
 	sd_write_name(buf, name);
 	sd_write_code(buf, sd_code::BLOB);
