apparmor/apparmor

commit 1b11f5be7e2f91a1c2a91babdd4915f339f37cba

parser: add override assign to cond list elements
Signed-off-by: Georgia Garcia <georgia.garcia@canonical.com>

parser/parser_lex.l
@@ -272,7 +272,7 @@ LIST_VALUE_ID_CHARS	([^ \t\n"!,]{-}[()]|\\[ ]|\\\t|\\\"|\\!|\\,|\\\(|\\\))
 LIST_VALUE_QUOTED_ID_CHARS [^\0"]|\\\"
 LIST_VALUE_ID	{LIST_VALUE_ID_CHARS}+
 QUOTED_LIST_VALUE_ID	\"{LIST_VALUE_QUOTED_ID_CHARS}+\"
-ID_CHARS_NOEQ	[^ \t\n"!,]{-}[=)]
+ID_CHARS_NOEQ	[^ \t\n"!,]{-}[:=)]
 LEADING_ID_CHARS_NOEQ [^ \t\n"!,]{-}[=()+&]
 ID_NOEQ		{ID_CHARS_NOEQ}|(,{ID_CHARS_NOEQ})
 IDS_NOEQ	{LEADING_ID_CHARS_NOEQ}{ID_NOEQ}*
@@ -484,6 +484,10 @@ IN_OP		in
 
 	{EQUALS} { RETURN_TOKEN(TOK_EQUALS); }
 
+	{OVERRIDE_ASSIGN}{WS}*/[^(\n]{-}{WS} { BEGIN_AND_RETURN(SUB_VALUE, TOK_OVERRIDE_ASSIGN); }
+
+	{OVERRIDE_ASSIGN} { RETURN_TOKEN(TOK_OVERRIDE_ASSIGN); }
+
 	/* Don't push state here as this is a transition start condition and
 	 * we want to return to the start condition that invoked <EXTCOND_MODE>
 	 * when LIST_VAL_ID is done
@@ -525,6 +529,14 @@ IN_OP		in
 
 	{EQUALS}	{
 		PUSH_AND_RETURN(LIST_COND_VAL, TOK_EQUALS);
+	}
+
+	{OVERRIDE_ASSIGN}{WS}*{OPEN_PAREN}	{
+		PUSH_AND_RETURN(LIST_COND_PAREN_VAL, TOK_OVERRIDE_ASSIGN);
+	}
+
+	{OVERRIDE_ASSIGN}	{
+		PUSH_AND_RETURN(LIST_COND_VAL, TOK_OVERRIDE_ASSIGN);
 	}
 }
 

parser/parser.h
@@ -120,6 +120,7 @@ enum class cond_comp {
 	NONE,
 	EQ,
 	IN,
+	SET,
 };
 
 struct cond_entry {

parser/parser_yacc.y
@@ -1320,6 +1320,20 @@ cond: TOK_CONDID TOK_EQUALS TOK_VALUE
 		$$ = ent;
 	}
 
+cond: TOK_CONDID TOK_OVERRIDE_ASSIGN TOK_VALUE
+	{
+		struct cond_entry *ent;
+		struct value_list *value = new_value_list($3);
+		if (!value)
+			yyerror(_("Memory allocation error."));
+		ent = new_cond_entry($1, cond_comp::SET, value);
+		if (!ent) {
+			free_value_list(value);
+			yyerror(_("Memory allocation error."));
+		}
+		$$ = ent;
+	}
+
 cond: TOK_CONDID TOK_EQUALS TOK_OPENPAREN valuelist TOK_CLOSEPAREN
 	{
 		struct cond_entry *ent = new_cond_entry($1, cond_comp::EQ, $4);
@@ -1333,6 +1347,15 @@ cond: TOK_CONDID TOK_EQUALS TOK_OPENPAREN valuelist TOK_CLOSEPAREN
 cond: TOK_CONDID TOK_IN TOK_OPENPAREN valuelist TOK_CLOSEPAREN
 	{
 		struct cond_entry *ent = new_cond_entry($1, cond_comp::IN, $4);
+
+		if (!ent)
+			yyerror(_("Memory allocation error."));
+		$$ = ent;
+	}
+
+cond: TOK_CONDID TOK_OVERRIDE_ASSIGN TOK_OPENPAREN valuelist TOK_CLOSEPAREN
+	{
+		struct cond_entry *ent = new_cond_entry($1, cond_comp::SET, $4);
 
 		if (!ent)
 			yyerror(_("Memory allocation error."));

commit 012cff31517d80aded814b2eee5054794ac21c72

Fix regression in mount parsing caused by overriding assignment
The change to allow overriding assignment expression in lists so that
the label could be assigned, eg.

  inet stream label:=non=default,

was too braod and broke parsing of list elements that contained
colons. eg. the rule

  mount fstype=nfs{,4} *:** -> /var/snap/{@{ONE},@{TWO}}/@{THREE}/{,**},

is broken because it contains *:** for the source mount.

Fix this by only limiting the immediate := combination as part of a
list element. If := is needed as an actual element it can be escaped or
embedded inside of quotes.

Closes: https://gitlab.com/apparmor/apparmor/-/work_items/624
Fixes: 1b11f5be7 ("parser: add override assign to cond list elements")
Signed-off-by: John Johansen <john.johansen@canonical.com>

parser/parser_lex.l
@@ -272,10 +272,17 @@ LIST_VALUE_ID_CHARS	([^ \t\n"!,]{-}[()]|\\[ ]|\\\t|\\\"|\\!|\\,|\\\(|\\\))
 LIST_VALUE_QUOTED_ID_CHARS [^\0"]|\\\"
 LIST_VALUE_ID	{LIST_VALUE_ID_CHARS}+
 QUOTED_LIST_VALUE_ID	\"{LIST_VALUE_QUOTED_ID_CHARS}+\"
-ID_CHARS_NOEQ	[^ \t\n"!,]{-}[:=)]
-LEADING_ID_CHARS_NOEQ [^ \t\n"!,]{-}[=()+&]
-ID_NOEQ		{ID_CHARS_NOEQ}|(,{ID_CHARS_NOEQ})
-IDS_NOEQ	{LEADING_ID_CHARS_NOEQ}{ID_NOEQ}*
+
+ID_CHARS_NOEQCOLON	[^ \t\n"!,]{-}[=:)]
+ID_CHARS_NOEQ		[^ \t\n"!,]{-}[=)]
+LEADING_ID_CHARS_NOEQCOLON [^ \t\n"!,]{-}[=()+&:]
+LEADING_ID_CHARS_NOEQ 	[^ \t\n"!,]{-}[=()+&]
+
+ID_NOASSIGN		({ID_CHARS_NOEQCOLON}|[:]{ID_CHARS_NOEQ}|(,{ID_CHARS_NOEQCOLON}))
+LEADING_ID_NOASSIGN	({LEADING_ID_CHARS_NOEQCOLON}|[:]{LEADING_ID_CHARS_NOEQ})
+IDS_NOASSIGN		{LEADING_ID_NOASSIGN}{ID_NOASSIGN}*
+
+
 ALLOWED_QUOTED_ID 	[^\0"]|\\\"
 QUOTED_ID	\"{ALLOWED_QUOTED_ID}*\"
 
@@ -518,7 +525,7 @@ IN_OP		in
 
 	{COMMA}		{ EAT_TOKEN("listcond: , \n"); }
 
-	{ID_CHARS_NOEQ}+	{
+	{IDS_NOASSIGN}	{
 		yylval.id = processid(yytext, yyleng);
 		RETURN_TOKEN(TOK_CONDID);
 	}
@@ -680,7 +687,7 @@ IN_OP		in
 }
 
 <MOUNT_MODE,DBUS_MODE,SIGNAL_MODE,PTRACE_MODE,UNIX_MODE,MQUEUE_MODE,IOURING_MODE,NETWORK_MODE>{
-	({IDS_NOEQ}|{LABEL}|{QUOTED_ID}) {
+	({IDS_NOASSIGN}|{LABEL}|{QUOTED_ID}) {
 		yylval.id = processid(yytext, yyleng);
 		RETURN_TOKEN(TOK_ID);
 	}
@@ -754,7 +761,7 @@ IN_OP		in
 	{LT}		{ RETURN_TOKEN(TOK_LT); }
 	{LT_EQUAL}	{ RETURN_TOKEN(TOK_LE); }
 
-	({IDS_NOEQ}|{LABEL}|{QUOTED_ID}) {
+	({IDS_NOASSIGN}|{LABEL}|{QUOTED_ID}) {
 		yylval.id = processid(yytext, yyleng);
 		RETURN_TOKEN(TOK_ID);
 	}

parser/tst/simple_tests/mount/regression1.sd
@@ -1,0 +1,11 @@
+#
+#=Description test that mount rule list element with embedded colon parses
+#=EXRESULT PASS
+
+@{ONE}=one
+@{TWO}=two
+@{THREE}=three
+
+/usr/bin/foo {
+  mount fstype=nfs{,4} *:** -> /var/snap/{@{ONE},@{TWO}}/@{THREE}/{,**},
+}

parser/tst/simple_tests/mount/regression2.sd
@@ -1,0 +1,11 @@
+#
+#=Description test that := can be embedded in list element when embedded in quotes
+#=EXRESULT PASS
+
+@{ONE}=one
+@{TWO}=two
+@{THREE}=three
+
+/usr/bin/foo {
+  mount fstype=nfs{,4} "*:=**" -> /var/snap/{@{ONE},@{TWO}}/@{THREE}/{,**},
+}

parser/tst/simple_tests/mount/regression3.sd
@@ -1,0 +1,12 @@
+#
+#=Description basic rules to test breaking := with an escape. Ideally this would work but the escape isn't being handled correctly
+#=EXRESULT PASS
+#=TODO
+
+@{ONE}=one
+@{TWO}=two
+@{THREE}=three
+
+/usr/bin/foo {
+  mount fstype=nfs{,4} *:\=** -> /var/snap/{@{ONE},@{TWO}}/@{THREE}/{,**},
+}

utils/test/test-parser-simple-tests.py
@@ -456,6 +456,11 @@ syntax_failure = (
     'network/network_ok_46.sd',
     'network/skb/ok_setlabel_peer_1.sd',
     'network/port-torture.sd',
+
+    # : in list condition and overriding assign
+    'mount/regression1.sd',
+    'mount/regression2.sd',
+    'mount/regression3.sd',
 )
 
 