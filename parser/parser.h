382e9770ee314ea4da54a08189b87cb23707e1db
Georgia Garcia <georgia.garcia@canonical.com>
2026-06-26 10:56:24 -0300

    parser: don't generate compat_net table if netv8 or netv9
    The compat_net table was being generated regardless if they were going
    to be used by parser_interface later. Add a condition to stop that
    from happening. Also, parser_interface was only checking for netv8 to
    determine if compat_net should be emitted, so now we have a
    features_supports_network_policydb to track it in case new network
    versions are created in the future.
    
    Signed-off-by: Georgia Garcia <georgia.garcia@canonical.com>
    (cherry picked from commit 671327e6a84a2e19b441eff33e8ec371b20be486,
     adapted for 4.1: features_supports_network_policydb is set from
     networkv8 only, the v9/skb/iface feature probes do not exist here)
    Signed-off-by: Maxime Bélair <maxime.belair@canonical.com>

--- parser/parser.h
+++ parser/parser.h
@@ -342,6 +342,7 @@ extern int kernel_load;
 extern int kernel_supports_setload;
 extern int features_supports_network;
 extern int features_supports_networkv8;
+extern bool features_supports_network_policydb;
 extern int features_supports_inet;
 extern int kernel_supports_policydb;
 extern bool kernel_supports_diff_encode;
