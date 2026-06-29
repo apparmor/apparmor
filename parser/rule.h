apparmor/apparmor

commit b27fcf10622350f240d09e74f543336ce38a3e4a

parser: fix deny/auditing prefixes on block rules
When implying the audit specifier from the deny rule, the parser was
not considering the value that was already in the rule, causing
different behaviors when dealing with block modifiers vs using the
same accumulated modifiers.

In practical terms, the following profiles would give different
results if rule_t rules in blocks were supported by the parser. (They
aren't, but it's coming soon).

/t { deny { audit signal, } }
/t { audit deny signal, }

Signed-off-by: Georgia Garcia <georgia.garcia@canonical.com>

parser/rule.h
@@ -161,10 +161,48 @@ std::ostream &operator<<(std::ostream &os, rule_t &rule);
 typedef std::list<rule_t *> RuleList;
 
 /* Scoped enums used in the bison front end */
-enum class audit_t { UNSPECIFIED, FORCE, QUIET };
+enum class audit_t { UNSPECIFIED, IMPLIED, FORCE, QUIET };
 enum class rule_mode_t { UNSPECIFIED, ALLOW, DENY, PROMPT };
 enum class owner_t { UNSPECIFIED, SPECIFIED, NOT };
 
+class internal_audit_t {
+public:
+	audit_t audit;
+	operator audit_t() const {
+		return audit;
+	}
+
+	internal_audit_t& operator=(const audit_t& rhs) {
+		audit = rhs;
+		return *this;
+	}
+
+	bool operator==(const audit_t& rhs) const {
+		if (rhs == audit_t::FORCE)
+			return (this->audit == rhs) || (this->audit == audit_t::IMPLIED);
+		return this->audit == rhs;
+	}
+
+	bool operator!=(const audit_t& rhs) const {
+		return !(*this == rhs);
+	}
+
+	bool operator==(const internal_audit_t& rhs) const {
+		return *this == rhs.audit;
+	}
+
+	bool operator!=(const internal_audit_t& rhs) const {
+		return !(*this == rhs);
+	}
+
+	bool operator-(const internal_audit_t& rhs) const {
+		return (int) this->audit - (int) rhs.audit;
+	}
+
+	bool operator<(const internal_audit_t& rhs) const {
+		return this->audit < rhs.audit;
+	}
+};
 
 /* NOTE: we can not have a constructor for class prefixes. This is
  * because it will break bison, and we would need to transition to
@@ -175,7 +213,7 @@ enum class owner_t { UNSPECIFIED, SPECIFIED, NOT };
 class prefixes {
 public:
 	int priority;
-	audit_t audit;
+	internal_audit_t audit;
 	rule_mode_t rule_mode;
 	owner_t owner;
 
@@ -249,7 +287,7 @@ public:
 		int tmp = priority - rhs.priority;
 		if (tmp != 0)
 			return tmp;
-		tmp = (int) audit - (int) rhs.audit;
+		tmp = audit - rhs.audit;
 		if (tmp != 0)
 			return tmp;
 		tmp = (int) rule_mode - (int) rhs.rule_mode;
@@ -301,6 +339,7 @@ public:
 		/* audit conflicts */
 		if (p.audit != audit_t::UNSPECIFIED) {
 			if (audit != audit_t::UNSPECIFIED &&
+			    audit != audit_t::IMPLIED &&
 			    audit != p.audit) {
 				error = "conflicting audit prefix";
 				return false;
@@ -308,7 +347,7 @@ public:
 //			audit = p.audit;
 		}
 
-		/* allow deny conflicts */
+		/* allow deny prompt conflicts */
 		if (p.rule_mode != rule_mode_t::UNSPECIFIED) {
 			if (rule_mode != rule_mode_t::UNSPECIFIED &&
 			    rule_mode != p.rule_mode) {
@@ -330,11 +369,13 @@ public:
 
 		/* TODO: MOVE this ! */
 		/* does the prefix imply a modifier */
-		if (p.rule_mode == rule_mode_t::DENY && p.audit == audit_t::FORCE) {
+		if ((p.rule_mode == rule_mode_t::DENY || rule_mode == rule_mode_t::DENY) &&
+		    (p.audit.audit == audit_t::FORCE || audit.audit == audit_t::FORCE)) {
 			rule_mode = rule_mode_t::DENY;
+			audit = audit_t::UNSPECIFIED;
 		} else if (p.rule_mode == rule_mode_t::DENY) {
 			rule_mode = rule_mode_t::DENY;
-			audit = audit_t::FORCE;
+			audit = audit_t::IMPLIED;
 		} else if (p.audit != audit_t::UNSPECIFIED) {
 			audit = p.audit;
 		}

commit b3c1f9bb6392758ffcb52779ee0a9026b7c3cbd2

parser: fix return value of operator- in internal_audit_t
Due to a copy-paste error, the operator- is returning a bool, leading
to undefined behavior when used in std::sort, which requires
strict-weak-ordering.

Fixes: b27fcf1062 ("parser: fix deny/auditing prefixes on block rules")
Signed-off-by: Georgia Garcia <georgia.garcia@canonical.com>

parser/rule.h
@@ -195,7 +195,7 @@ public:
 		return !(*this == rhs);
 	}
 
-	bool operator-(const internal_audit_t& rhs) const {
+	int operator-(const internal_audit_t& rhs) const {
 		return (int) this->audit - (int) rhs.audit;
 	}
 