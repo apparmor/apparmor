apparmor/apparmor

commit 05d3c4fdaca4a5306659ff3438a34e0b3c2eb071

parser: fix file rules to identify conflicts on prefixes
The parser is now capable of identifying conflicts on file rules. In
addition, fix deny/auditing prefixes on block rules.

The following profiles would compile into different things, which
should not be the case:
/t { deny { audit /foo r, } }
/t { audit deny /foo r, }

Signed-off-by: Georgia Garcia <georgia.garcia@canonical.com>

parser/parser.h
@@ -150,7 +150,7 @@ struct cod_entry {
 	Profile *prof;		 	/* Special profile defined
 					 * just for this executable */
 	perm32_t perms;			/* perms is 'or' of AA_* bits */
-	audit_t audit;
+	internal_audit_t audit;
 	rule_mode_t rule_mode;
 
 	bool alias_ignore;		/* ignore for alias processing */

parser/parser_misc.c
@@ -1106,13 +1106,15 @@ bool check_x_qualifier(struct cod_entry *entry, const char *&error)
 // cod_entry version of ->add_prefix here just as file rules aren't converted yet
 bool entry_add_prefix(struct cod_entry *entry, const prefixes &p, const char *&error)
 {
-	/* modifiers aren't correctly stored for cod_entries yet so
-	 * we can't conflict on them easily. Leave that until conversion
-	 * to rule_t
-	 */
 	/* apply rule mode */
-	if (p.rule_mode != rule_mode_t::UNSPECIFIED)
+	if (p.rule_mode != rule_mode_t::UNSPECIFIED) {
+		if (entry->rule_mode != rule_mode_t::UNSPECIFIED &&
+		    entry->rule_mode != p.rule_mode) {
+			error = "conflicting mode prefix";
+			return false;
+		}
 		entry->rule_mode = p.rule_mode;
+	}
 
 	/* apply owner/other */
 	if (p.owner == owner_t::SPECIFIED)
@@ -1123,11 +1125,25 @@ bool entry_add_prefix(struct cod_entry *entry, const prefixes &p, const char *&e
 	if (p.priority != 0)
 		entry->priority = p.priority;
 
+	if (p.audit != audit_t::UNSPECIFIED) {
+		if (entry->audit != audit_t::UNSPECIFIED &&
+		    entry->audit != audit_t::IMPLIED &&
+		    entry->audit != p.audit) {
+			error = "conflicting audit prefix";
+			return false;
+		}
+	}
 	/* implied audit modifier */
-	if (p.audit == audit_t::FORCE && (p.rule_mode != rule_mode_t::DENY))
-		entry->audit = audit_t::FORCE;
-	else if (p.audit != audit_t::FORCE && (p.rule_mode == rule_mode_t::DENY))
-		entry->audit = audit_t::FORCE;
+	if ((p.rule_mode == rule_mode_t::DENY || entry->rule_mode == rule_mode_t::DENY) &&
+	    (p.audit.audit == audit_t::FORCE || entry->audit.audit == audit_t::FORCE)) {
+		entry->rule_mode = rule_mode_t::DENY;
+		entry->audit = audit_t::UNSPECIFIED;
+	} else if (p.rule_mode == rule_mode_t::DENY) {
+		entry->rule_mode = rule_mode_t::DENY;
+		entry->audit = audit_t::IMPLIED;
+	} else if (p.audit != audit_t::UNSPECIFIED) {
+		entry->audit = p.audit;
+	}
 
 	return check_x_qualifier(entry, error);
 }

parser/tst/equality.sh
@@ -1365,6 +1365,58 @@ test_parser_variables()
 
 }
 
+test_block_configurations()
+{
+	######## prefix ####### allow, deny, prompt, audit, owner
+	### allow ###
+	# deny and prompt not used since it conflicts with allow
+	verify_binary_equality "allow expands correctly - file" \
+			       "/t { allow { /foo r, audit /bar w, owner /baz rw, } }" \
+			       "/t { allow /foo r, audit /bar w, owner /baz rw, }"
+
+	verify_binary_equality "allow expands correctly in subblock - file" \
+			       "/t { allow { { /foo r, audit /bar w, owner /baz rw, } } }" \
+			       "/t { allow /foo r, audit /bar w, owner /baz rw, }"
+
+	### deny ###
+	# allow and prompt  not used since it conflicts with deny
+	verify_binary_equality "deny expands correctly - file" \
+			       "/t { deny { /foo r, audit /bar w, owner /baz rw, } }" \
+			       "/t { deny /foo r, audit deny /bar w, deny owner /baz rw, }"
+
+	verify_binary_equality "deny expands correctly in subblock - file" \
+			       "/t { deny { { /foo r, audit /bar w, owner /baz rw, } } }" \
+			       "/t { deny /foo r, audit deny /bar w, deny owner /baz rw, }"
+
+	### prompt ###
+	# allow and deny not used since it conflicts with prompt
+	verify_binary_equality "prompt expands correctly - file" \
+			       "/t { prompt { /foo r, audit /bar w, owner /baz rw, } }" \
+			       "/t { prompt /foo r, audit prompt /bar w, prompt owner /baz rw, }"
+
+	verify_binary_equality "prompt expands correctly in subblock - file" \
+			       "/t { prompt { { /foo r, audit /bar w, owner /baz rw, } } }" \
+			       "/t { prompt /foo r, audit prompt /bar w, prompt owner /baz rw, }"
+
+	### audit ###
+	verify_binary_equality "audit expands correctly - file" \
+			       "/t { audit { /foo r, allow /foo w, deny /bar r, audit /bar w, prompt /baz r, owner /baz w, } }" \
+			       "/t { audit /foo rw, audit deny /bar r, audit /bar w, audit prompt /baz r, audit owner /baz w, }"
+
+	verify_binary_equality "audit expands correctly in subblock - file" \
+			       "/t { audit { { /foo r, allow /foo w, deny /bar r, audit /bar w, prompt /baz r, owner /baz w, } } }" \
+			       "/t { audit /foo rw, audit deny /bar r, audit /bar w, audit prompt /baz r, audit owner /baz w, }"
+
+	### owner ###
+	verify_binary_equality "owner expands correctly - file" \
+			       "/t { owner { /foo r, allow /foo w, deny /bar r, audit /bar w, prompt /baz r, owner /baz w, } }" \
+			       "/t { owner /foo rw, deny owner /bar r, audit owner /bar w, prompt owner /baz r, owner /baz w, }"
+
+	verify_binary_equality "owner expands correctly in subblock - file" \
+			       "/t { owner { { /foo r, allow /foo w, deny /bar r, audit /bar w, prompt /baz r, owner /baz w, } } }" \
+			       "/t { owner /foo rw, deny owner /bar r, audit owner /bar w, prompt owner /baz r, owner /baz w, }"
+}
+
 run_tests()
 {
 	printf "Equality Tests:\n"
@@ -1544,6 +1596,8 @@ run_tests()
 					/t @{ROOT}@{BAR}/@{FOO} { }"
 
 	test_parser_variables
+
+	test_block_configurations
 
 	# verify combinations of different priority levels
 	# for single rule comparisons, rules should keep same expected result

commit 612fbd896335b638beb3be2e5d724e11776e0009

parser: fix change_profile prefix handling
Since change_profile handled its own prefix changes, it got left out
of 05d3c4fdac. Unify them into a single generic_add_prefix which can
be used by entry_add_prefix and change_profile directly. They had to
be split because change_profile does not support the owner prefix or
can have its x qualifiers checked.

Fixes: 05d3c4fdac ("parser: fix file rules to identify conflicts on prefixes")
Signed-off-by: Georgia Garcia <georgia.garcia@canonical.com>

parser/parser.h
@@ -488,6 +488,7 @@ extern struct cod_entry *copy_cod_entry(struct cod_entry *cod);
 extern void free_cod_entries(struct cod_entry *list);
 extern void debug_cod_entries(struct cod_entry *list);
 extern bool check_x_qualifier(struct cod_entry *entry, const char *&error);
+extern bool generic_add_prefix(struct cod_entry *entry, const prefixes &p, const char *&error);
 extern bool entry_add_prefix(struct cod_entry *entry, const prefixes &p, const char *&error);
 
 

parser/parser_misc.c
@@ -1103,8 +1103,10 @@ bool check_x_qualifier(struct cod_entry *entry, const char *&error)
 	return true;
 }
 
-// cod_entry version of ->add_prefix here just as file rules aren't converted yet
-bool entry_add_prefix(struct cod_entry *entry, const prefixes &p, const char *&error)
+/* change_profile follows the same rules as file add_prefix except for
+ * owner and check_x_qualifier, so the generic elements were
+ * extracted while cod_entry is not converted to rule_t */
+bool generic_add_prefix(struct cod_entry *entry, const prefixes &p, const char *&error)
 {
 	/* apply rule mode */
 	if (p.rule_mode != rule_mode_t::UNSPECIFIED) {
@@ -1115,12 +1117,6 @@ bool entry_add_prefix(struct cod_entry *entry, const prefixes &p, const char *&e
 		}
 		entry->rule_mode = p.rule_mode;
 	}
-
-	/* apply owner/other */
-	if (p.owner == owner_t::SPECIFIED)
-		entry->perms &= (AA_USER_PERMS | AA_SHARED_PERMS);
-	else if (p.owner == owner_t::NOT)
-		entry->perms &= (AA_OTHER_PERMS | AA_SHARED_PERMS);
 
 	if (p.priority != 0)
 		entry->priority = p.priority;
@@ -1144,6 +1140,20 @@ bool entry_add_prefix(struct cod_entry *entry, const prefixes &p, const char *&e
 	} else if (p.audit != audit_t::UNSPECIFIED) {
 		entry->audit = p.audit;
 	}
+	return true;
+}
+
+// cod_entry version of ->add_prefix here just as file rules aren't converted yet
+bool entry_add_prefix(struct cod_entry *entry, const prefixes &p, const char *&error)
+{
+	if (!generic_add_prefix(entry, p, error))
+		return false;
+
+	/* apply owner/other */
+	if (p.owner == owner_t::SPECIFIED)
+		entry->perms &= (AA_USER_PERMS | AA_SHARED_PERMS);
+	else if (p.owner == owner_t::NOT)
+		entry->perms &= (AA_OTHER_PERMS | AA_SHARED_PERMS);
 
 	return check_x_qualifier(entry, error);
 }

parser/parser_yacc.y
@@ -808,20 +808,17 @@ rules:  rules opt_prefix prefix_rule
 
 rules:	rules opt_prefix change_profile
 	{
+		const char *error;
 		PDEBUG("matched: rules change_profile\n");
 		PDEBUG("rules change_profile: (%s)\n", $3->name);
 		if (!$3)
 			yyerror(_("Assert: `change_profile' returned NULL."));
+
 		if ($2.owner != owner_t::UNSPECIFIED)
 			yyerror(_("owner conditional not allowed on unix rules"));
-		if (($2.rule_mode == rule_mode_t::DENY) && $2.audit == audit_t::FORCE) {
-			$3->rule_mode = rule_mode_t::DENY;
-		} else if ($2.rule_mode == rule_mode_t::DENY) {
-			$3->rule_mode = rule_mode_t::DENY;
-			$3->audit = audit_t::FORCE;
-		} else if ($2.audit != audit_t::UNSPECIFIED) {
-			$3->audit = $2.audit;
-		}
+		if (!generic_add_prefix($3, $2, error))
+			yyerror("%s", error);
+
 		add_entry_to_policy($1, $3);
 		$$ = $1;
 	};