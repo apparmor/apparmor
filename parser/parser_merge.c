apparmor/apparmor

commit 1bf05d8d07a9823fe9f2acc4b5a38b837b46df78

parser: for the file rule comparison don't call strlen() each time
Currently the file rule comparison is calling strlen multiple times,
instead do it once when setting up the sort

Signed-off-by: John Johansen <john.johansen@canonical.com>

parser/parser_merge.c
@@ -25,20 +25,17 @@
 #include "parser.h"
 #include "profile.h"
 
-static int strrcmp(const char *a, const char *b)
+static int strrcmp(const char *a, int ta,
+		   const char *b, int tb)
 {
-	int la = strlen(a);
-	int lb = strlen(b);
-
-	while (la && lb) {
-		if (a[la] < b[lb])
-			return -1;
-		if (a[la--] > b[lb--])
-			return 1;
+	while (ta && tb) {
+		int res = (int) a[ta--] - (int) b[tb--];
+		if (res)
+			return res;
 	}
-	if (la < lb)
+	if (ta < tb)
 		return -1;
-	if (la > lb)
+	if (ta > tb)
 		return 1;
 	return 0;
 }
@@ -52,7 +49,8 @@ static int file_comp(const void *c1, const void *c2)
 
 	if ((*e1)->link_name) {
 		if ((*e2)->link_name)
-			res = strrcmp((*e1)->link_name, (*e2)->link_name);
+			res = strrcmp((*e1)->link_name, (*e1)->llen,
+				      (*e2)->link_name, (*e2)->llen);
 		else
 			return 1;
 	} else if ((*e2)->link_name) {
@@ -75,7 +73,8 @@ static int file_comp(const void *c1, const void *c2)
 	if ((*e1)->priority != (*e2)->priority)
 		return (*e2)->priority - (*e1)->priority;
 
-	return strrcmp((*e1)->name, (*e2)->name);
+	return strrcmp((*e1)->name, (*e1)->len,
+		       (*e2)->name, (*e2)->len);
 }
 
 static int process_file_entries(Profile *prof)
@@ -96,8 +95,11 @@ static int process_file_entries(Profile *prof)
 		return -ENOMEM;
 	}
 
-	for (cur = prof->entries, n = 0; cur; cur = cur->next, n++)
+	for (cur = prof->entries, n = 0; cur; cur = cur->next, n++) {
 		table[n] = cur;
+		cur->len = strlen(cur->name);
+		cur->llen = cur->link_name ? strlen(cur->link_name) : 0;
+	}
 	qsort(table, count, sizeof(struct cod_entry *), file_comp);
 	table[count] = NULL;
 	for (n = 0; n < count; n++)

parser/parser.h
@@ -140,6 +140,8 @@ struct cond_entry_list {
 struct cod_entry {
 	int priority;
 	char *name;
+	int len;
+	int llen;
 	union {
 		char *link_name;
 		char *onexec;

parser/parser_misc.c
@@ -977,7 +977,9 @@ struct cod_entry *new_entry(char *id, perm32_t perms, char *link_id)
 
 	entry->priority = 0;
 	entry->name = id;
+	entry->len = 0;
 	entry->link_name = link_id;
+	entry->llen = 0;
 	entry->perms = perms;
 	entry->audit = audit_t::UNSPECIFIED;
 	entry->rule_mode = rule_mode_t::UNSPECIFIED;

commit 3c1966c0e89b32d25ca022ea4b7b070197c61073

parser: Fix file merge comparison to use all characters in the str
Currently the file merge comparison skips the first character in the
string at position 0. Currently this is always '/', however this is
brittle, and if we ever change this, we have an error that will be
hard to track down.

Fix this by making sure we compare str[0] before breaking from the loop

Closes: https://gitlab.com/apparmor/apparmor/-/work_items/644
Fixes: 1bf05d8d0 ("parser: for the file rule comparison don't call strlen() each time")
Signed-off-by: John Johansen <john.johansen@canonical.com>

parser/parser_merge.c
@@ -28,7 +28,7 @@
 static int strrcmp(const char *a, int ta,
 		   const char *b, int tb)
 {
-	while (ta && tb) {
+	while (ta >= 0 && tb >= 0) {
 		int res = (int) a[ta--] - (int) b[tb--];
 		if (res)
 			return res;